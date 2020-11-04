f82397fad50b654fc8ca84f94b6825dbebee284e
Markku Kero <mkx@eqela.com>
2020-11-05 00:15:49 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -53,6 +53,7 @@ fi
 # Install the downloaded file
 TARGETDIR="$VMDIR/$LATESTVERSION"
 rm -rf "$TARGETDIR"
+mkdir -p "$VMDIR"
 unzip "$DOWNLOADEDFILE" -d "$TARGETDIR" || exit 1
 rm -f "$BINDIR/sushi"
 mkdir -p "$BINDIR"
